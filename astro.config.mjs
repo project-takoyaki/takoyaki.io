1d8a17fb76ccf27b81c89eee8fd33862eadfaa68
Mewski <mewski@mewski.dev>
2026-02-14 00:03:51 -0600

    chore: fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mewski.pages.mewski.dev",
+  base: "/takoyaki.io",
   vite: {
     plugins: [tailwindcss()],
   },
